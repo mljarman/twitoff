4f630a910d2decf3225d3a8643442448b5e054ab
mljarman <mljarman@users.noreply.github.com>
2019-12-16 11:30:53 -0700

    intro lesson folder

---
+++ intro_lesson/hello.py
@@ -0,0 +1,22 @@
+"""
+Minimal Flask App
+"""
+
+from flask import Flask, render_template
+
+# Make the application
+app = Flask(__name__)
+
+# Make the route
+@app.route("/")
+
+# Now define a function
+def hello():
+    return "Hello World!"
+
+# Make a second route
+@app.route("/about")
+
+# Now make the function that goes with about
+def preds():
+    return render_template('about.html')
