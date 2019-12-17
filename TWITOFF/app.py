3ba720f3a8fbef240e0d170b7bd607b5b3b43b80
mljarman <mljarman@users.noreply.github.com>
2019-12-17 11:44:53 -0700

    create twitter.py and update pages

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -2,20 +2,26 @@
 Code for our app.
 """
 
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 # Make app factory:
 def create_app():
     app = Flask(__name__)
 
     # Add config for database:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABSE_URL')
+
+    # Stop tracking modifications on sqlalchemy config:
+    app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
+
     # Have the database know about the app:
     DB.init_app(app)
 
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff!!'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
     return app
